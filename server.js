2f2440b95d9c7e1c2d9e5b7fc6d4a0f203ecd2ee
riachh <59855086+riachh@users.noreply.github.com>
2022-04-21 00:00:58 -0400

    Trial #??

--- server.js
+++ server.js
@@ -6,32 +6,17 @@ const args = require('minimist')(process.argv.slice(2))
 args["port"]
 
 //Start Listening 
-const HTTP_PORT = args.port || 5000
+const HTTP_PORT = args.port || process.env.PORT || 5000
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
-
-//Default Response 
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
-
-app.get('/app/echo/:number', (req, res) => {
-    res.status(200).json({'message': req.params.number})
-})
-
 app.get('/app/', (req, res) => {
     res.statusCode = 200; 
-    // Respond with status message "OK"
+    //Respond with status message "OK"
     res.statusMessage = 'OK';
     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage)
-})
-
-app.get('/app/flip', (req, res) => {
-    var flip = coinFlip(); 
-    res.status(200).json({'flip' : flip})
+    res.end(res.statusCode + ' ' + res.statusMessage)
 })
 
 
@@ -86,3 +71,27 @@ function countFlips(array) {
     const resultss = { call: call, flip: flipp, result: winOrLose };
     return resultss; 
   }
+  //End Functions...
+
+  //Calls 
+  app.get('/app/flip/', (req, res) => {
+    res.statusCode = 200;
+    res.writeHead(res.statusCode, {'Content-Type' : 'text/plain'});
+    res.end('{"flip":"' + coinFlip() + '"}')
+  });
+
+  app.get('/app/flips/:number', (req, res) => {
+    var flip = coinFlips(req.params.number)
+    var num = countFlips(flip)
+    res.status(200).json({'flips' : flip, 'count' : num})
+  });
+
+  app.get('/app/flip/call/:guess(heads|tails)/', (req, res) => {
+    const guesss = flipACoin(req.params.guess)
+    res.status(200).json(guesss)
+  });
+
+  //Default Response 
+  app.use(function(req, res) {
+    res.status(404).send('404 NOT FOUND')
+  });
